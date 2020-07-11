f2091196e821bbad8656b6745e6bf7a88318a544
Srinivas469 <68150455+Srinivas469@users.noreply.github.com>
2020-07-11 21:06:00 +0400

    Add files via upload

--- application.py
+++ application.py
@@ -17,6 +17,30 @@ def jumpredictchurnrisk():
         #All API Logic here
         return 'Predictchurrisk API here'
 
+@app.route('/dam/predictsegment', methods=['POST'])
+def dampredictsegment():
+    if request.method == "POST":
+        content = request.get_json()
+        df = pd.io.json.json_normalize(content)
+        return 'API Response here, can be json'
+
+@app.route('/dam/predictchurnrisk', methods=['POST'])
+def dampredictchurnrisk():
+    if request.method == "POST":
+        if request.authorization:
+            username = request.authorization.username
+            password = request.authorization.password
+        else:
+            return make_response('Basic Authentication not provided', 401, {'WWW-Authenticate': 'Basic-realm="Login Required"'})
+
+        if username != "khanmaaz" or password != "mypassword":
+            return make_response('Incorrect Basic Authentication', 401, {'WWW-Authenticate' : 'Basic-realm="Login Required"'})
+
+        content = request.get_json()
+        inputdf = pd.io.json.json_normalize(content)
+        noofrecs = inputdf.count()
+
+        return 'No of records is ' + str(noofrecs)
 
 if __name__ == '__main__':
     app.run()
